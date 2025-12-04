415d9181b1aeb614e3d61eeaeaca5495fb3e6d58
kaigidwani <kpg8244@rit.edu>
2025-12-04 17:26:55 -0500

    Implemented all dialogue scripts and dialogue trigger zones.

--- Media Narrative Proj/Assets/Scripts/MovementController.cs
+++ Media Narrative Proj/Assets/Scripts/MovementController.cs
@@ -34,7 +34,7 @@ public class MovementController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        gameObject.tag = "Player";
     }
 
     // Update is called once per frame
